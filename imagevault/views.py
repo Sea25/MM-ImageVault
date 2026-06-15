cf6c616b51994051a6d2d4282fd50650704e37fd
Sea25 <sonaabraham016@gmail.com>
2026-06-15 14:46:25 +0530

    Upload and Delete

--- imagevault/views.py
+++ imagevault/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import ImageFile
+import os
 
 def home(request):
-    return render(request, 'imagevault/home.html')
+    if request.method == 'POST':
+        images = request.FILES.getlist('images')
+
+        for image in images:
+            ImageFile.objects.create(image=image)
+        return redirect('home')
+
+    uploaded_images = ImageFile.objects.all().order_by('-uploaded_at')
+
+    return render(request, 'imagevault/home.html', {'uploaded_images': uploaded_images})
+
+def delete_image(equest, id):
+    image=get_object_or_404(ImageFile, id=id)
+    if image.image and os.path.isfile(image.image.path):
+        os.remove(image.image.path)
+    image.delete()
+    return redirect('home')
